23f4fce29f97469af2deb5f26a279ef4b6c34df4
Hakan <ahmethakanozturk34@gmail.com>
2023-02-16 17:25:56 +0300

    Performans Optimization
    - EF Core's default tracking mechanism is now selectable on ReadRepository

--- ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/Repositories/ReadRepository.cs
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/Repositories/ReadRepository.cs
@@ -4,10 +4,12 @@ using ETicaretAPI.Persistence.Contexts;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace ETicaretAPI.Persistence.Repositories
 {
@@ -22,14 +24,38 @@ namespace ETicaretAPI.Persistence.Repositories
 
         public DbSet<T> Table => _context.Set<T>();
 
-        public IQueryable<T> GetAll()
-            => Table;
-        public IQueryable<T> GetWhere(Expression<Func<T, bool>> metot)
-            => Table.Where(metot);
-        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> metot)
-            => await Table.FirstOrDefaultAsync(metot);
-        public async Task<T> GetByIdAsync(string id)
-            => await Table.FindAsync(Guid.Parse(id));
-            //=> await Table.FirstOrDefaultAsync(data => data.Id == Guid.Parse(id));
+        public IQueryable<T> GetAll(bool tracking)
+        {
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = query.AsNoTracking();
+            return query;
+        }
+        public IQueryable<T> GetWhere(Expression<Func<T, bool>> metot, bool tracking)
+        {
+            var query = Table.Where(metot).AsQueryable();
+            if (!tracking)
+                query = query.AsNoTracking();
+            return query;
+        }
+
+        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> metot, bool tracking)
+            //=> await GetAll(tracking).FirstOrDefaultAsync(metot);
+        {
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = query.AsNoTracking();
+            return await query.FirstOrDefaultAsync(metot);
+        }
+        
+        public async Task<T> GetByIdAsync(string id, bool tracking)
+        //=> await Table.FindAsync(Guid.Parse(id));
+        //=> await Table.FirstOrDefaultAsync(data => data.Id == Guid.Parse(id));
+        {
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = query.AsNoTracking();
+            return await query.FirstOrDefaultAsync(data => data.Id == Guid.Parse(id));
+        }
     }
 }
--- ETicaretAPI/Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ ETicaretAPI/Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -21,13 +21,19 @@ namespace ETicaretAPI.API.Controllers
         [HttpGet]
         public async Task Get()
         {
-            await _productWriteRepository.AddRangeAsync(new()
-            {
-                new (){ Id = Guid.NewGuid(), CreatedDate= DateTime.UtcNow, Name ="Product 1" , Price = 50, Stock= 10},
-                new (){ Id = Guid.NewGuid(), CreatedDate= DateTime.UtcNow, Name ="Product 2" , Price = 150, Stock= 20},
-                new (){ Id = Guid.NewGuid(), CreatedDate= DateTime.UtcNow, Name ="Product 3" , Price = 200, Stock= 30},
-            });
-            var a = await _productWriteRepository.SaveChanges();
+            //await _productWriteRepository.AddRangeAsync(new()
+            //{
+            //    new (){ Id = Guid.NewGuid(), CreatedDate= DateTime.UtcNow, Name ="Product 1" , Price = 50, Stock= 10},
+            //    new (){ Id = Guid.NewGuid(), CreatedDate= DateTime.UtcNow, Name ="Product 2" , Price = 150, Stock= 20},
+            //    new (){ Id = Guid.NewGuid(), CreatedDate= DateTime.UtcNow, Name ="Product 3" , Price = 200, Stock= 30},
+            //});
+            //var a = await _productWriteRepository.SaveChanges();
+
+            Product prod = await _productReadRepository.GetByIdAsync("070d9b6d-52fa-43b4-b82e-cd72a72b9708", false);
+            prod.Name = "Product 6";
+            await _productWriteRepository.SaveChanges();
+
+
         }
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(string id)
