9b122bb7459c49edca8a4bdf22d4c57c07364c5a
Hakan <ahmethakanozturk34@gmail.com>
2023-02-22 16:00:42 +0300

    Client Development
    - Add navbar

--- ETicaretClient/src/app/admin/layout/components/sidebar/sidebar.component.ts
+++ ETicaretClient/src/app/admin/layout/components/sidebar/sidebar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {MatListModule} from '@angular/material/list';
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
