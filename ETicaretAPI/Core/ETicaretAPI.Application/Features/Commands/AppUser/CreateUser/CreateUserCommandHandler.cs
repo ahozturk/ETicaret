1837d42573828783e94dd33c61ef4865a16f84a8
Hakan <ahmethakanozturk34@gmail.com>
2023-04-14 18:42:56 +0300

    User Create
    - Add User Creation
    - Add UserCreate for MediatR
    - Update UserController

---
+++ ETicaretAPI/Core/ETicaretAPI.Application/Features/Commands/AppUser/CreateUser/CreateUserCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using ETicaretAPI.Application.Exceptions;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace ETicaretAPI.Application.Features.Commands.AppUser.CreateUser
+{
+    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommandRequest, CreateUserCommandResponse>
+    {
+        readonly UserManager<Domain.Entities.Identity.AppUser> _userManager;
+
+        public CreateUserCommandHandler(UserManager<Domain.Entities.Identity.AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<CreateUserCommandResponse> Handle(CreateUserCommandRequest request, CancellationToken cancellationToken)
+        {
+            IdentityResult identityResult = await _userManager.CreateAsync(new()
+            {
+                Id = Guid.NewGuid().ToString(),
+                NameSurname = request.NameSurname,
+                UserName = request.Username,
+                Email = request.Email,
+            }, request.Password);
+
+            CreateUserCommandResponse response = new CreateUserCommandResponse() { Succeeded = identityResult.Succeeded};
+
+            if (identityResult.Succeeded)
+                response.Message = "Kullanıcı Başarıyla Oluşturulmuştur";
+            else
+                foreach (var error in identityResult.Errors)
+                    response.Message += $"{error.Code} - {error.Description}\n";
+
+            return response;
+        }
+    }
+}
---
+++ ETicaretAPI/Core/ETicaretAPI.Application/Features/Commands/AppUser/CreateUser/CreateUserCommandResponse.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Commands.AppUser.CreateUser
+{
+    public class CreateUserCommandResponse : IRequest<CreateUserCommandRequest>
+    {
+        public bool Succeeded { get; set; }
+        public string Message { get; set; }
+
+    }
+}
